a4038ea3d6fb0fb3396734d1d1670f2ac75f965e
Austin Karuru <karuruaustine@gmail.com>
2018-09-10 16:04:07 +0300

    1

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ import os
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:moringa1234@localhost/pomodoro'
 
 
 class ProdConfig(Config):
